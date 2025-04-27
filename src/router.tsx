d64ac1342c429996ee6cdecfa1e784fdc8e68e93
kch6396 <cjfghks6396@gmail.com>
2025-04-27 23:01:34 +0900

    chore

--- src/router.tsx
+++ src/router.tsx
@@ -6,7 +6,7 @@ import Home from "./Routes/Home";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/react-masterclass/netflix",
     element: <App />,
     children: [
       {
